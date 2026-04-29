dd688352303b8aa5b46815bb104e6c073109a75d
g-95272556-collab <g-95272556@moe-dl.edu.my>
2026-04-29 13:28:47 +0800

    fix: harden Netlify OAuth bootstrap

---
+++ runtime-config.js
@@ -0,0 +1,4 @@
+window.SMARTSCHOOLHUB_RUNTIME_CONFIG = Object.assign({
+  workerUrl: 'https://smartschoolhub-skkiandongo.g-95272556.workers.dev',
+  googleClientId: '553204925712-p975t8hnehd4vfhs3igf4ba9c63edf0f.apps.googleusercontent.com'
+}, window.SMARTSCHOOLHUB_RUNTIME_CONFIG || {});
